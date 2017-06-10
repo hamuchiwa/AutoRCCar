6a54a824b4388d8377b828fb3e86d9e6e2f57803
hamuchiwa <hamuchiwa@users.noreply.github.com>
2017-06-10 10:42:16 -0400

    update mlp_training.py
    merge train/test into one file

--- computer/mlp_training.py
+++ computer/mlp_training.py
@@ -3,6 +3,7 @@ __author__ = 'zhengwang'
 import cv2
 import numpy as np
 import glob
+from sklearn.cross_validation import train_test_split
 
 print 'Loading training data...'
 e0 = cv2.getTickCount()
@@ -14,22 +15,22 @@ training_data = glob.glob('training_data/*.npz')
 
 for single_npz in training_data:
     with np.load(single_npz) as data:
-        print data.files
         train_temp = data['train']
         train_labels_temp = data['train_labels']
-        print train_temp.shape
-        print train_labels_temp.shape
     image_array = np.vstack((image_array, train_temp))
     label_array = np.vstack((label_array, train_labels_temp))
 
-train = image_array[1:, :]
-train_labels = label_array[1:, :]
-print train.shape
-print train_labels.shape
+X = image_array[1:, :]
+y = label_array[1:, :]
+print 'Image array shape: ', X.shape
+print 'Label array shape: ', y.shape
 
 e00 = cv2.getTickCount()
 time0 = (e00 - e0)/ cv2.getTickFrequency()
 print 'Loading image duration:', time0
+
+# train test split, 7:3
+train, test, train_labels, test_labels = train_test_split(X, y, test_size=0.3)
 
 # set start time
 e1 = cv2.getTickCount()
@@ -39,7 +40,7 @@ layer_sizes = np.int32([38400, 32, 4])
 model = cv2.ANN_MLP()
 model.create(layer_sizes)
 criteria = (cv2.TERM_CRITERIA_COUNT | cv2.TERM_CRITERIA_EPS, 500, 0.0001)
-criteria2 = (cv2.TERM_CRITERIA_COUNT, 100, 0.001)
+#criteria2 = (cv2.TERM_CRITERIA_COUNT, 100, 0.001)
 params = dict(term_crit = criteria,
                train_method = cv2.ANN_MLP_TRAIN_PARAMS_BACKPROP,
                bp_dw_scale = 0.001,
@@ -52,18 +53,23 @@ num_iter = model.train(train, train_labels, None, params = params)
 e2 = cv2.getTickCount()
 time = (e2 - e1)/cv2.getTickFrequency()
 print 'Training duration:', time
+#print 'Ran for %d iterations' % num_iter
 
-# save param
+# train data
+ret_0, resp_0 = model.predict(train)
+prediction_0 = resp_0.argmax(-1)
+true_labels_0 = train_labels.argmax(-1)
+
+train_rate = np.mean(prediction_0 == true_labels_0)
+print 'Train accuracy: ', "{0:.2f}%".format(train_rate * 100)
+
+# test data
+ret_1, resp_1 = model.predict(test)
+prediction_1 = resp_1.argmax(-1)
+true_labels_1 = test_labels.argmax(-1)
+
+test_rate = np.mean(prediction_1 == true_labels_1)
+print 'Test accuracy: ', "{0:.2f}%".format(test_rate * 100)
+
+# save model
 model.save('mlp_xml/mlp.xml')
-
-print 'Ran for %d iterations' % num_iter
-
-ret, resp = model.predict(train)
-prediction = resp.argmax(-1)
-print 'Prediction:', prediction
-true_labels = train_labels.argmax(-1)
-print 'True labels:', true_labels
-
-print 'Testing...'
-train_rate = np.mean(prediction == true_labels)
-print 'Train rate: %f:' % (train_rate*100)
